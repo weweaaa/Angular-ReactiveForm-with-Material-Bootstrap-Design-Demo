6893ce5123e87e74896fb4254fe829576d2c2398
jia <jia@miniasp.com>
2020-02-11 11:22:03 +0800

    新增：基礎環境建置 新增：基礎框架建立       - header conponent       - main conponent       - menu conponent       - footer conponent 新增：基本套件引用 [@angular/material]、[@ng-bootstrap/ng-bootstrap] 修正：material 在 9.0.0 時會有一些小問題，所以先降板到 8.2.3 搭配 Angualr cdk 也降版到 8.2.3 [使用命令 npm install @angular/cdk@8.2.3 --save]       - 來源文章 https://stackoverflow.com/questions/58594311/angular-material-index-d-ts-is-not-a-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatDialogModule, MatInputModule, MatDialogRef, MatButtonModule, MAT_DIALOG_DATA } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './page/header/header.component';
+import { MainComponent } from './page/main/main.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { FooterComponent } from './page/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
