2083f9d57f10cb07f5b0cfa2fb82245d9942212b
jia <jia@miniasp.com>
2020-02-12 09:30:36 +0800

    調整：移除多餘的檔案

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './page/header/header.component';
 import { MainComponent } from './page/main/main.component';
@@ -33,7 +32,6 @@ import { MaterialModule } from './material.module';
     appRouting,
 
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
 
     MaterialModule
