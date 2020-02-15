ce5257e974bc5bea06669d3220351efc15231619
jia <jia@miniasp.com>
2020-02-15 10:27:37 +0800

    新增：完成按鈕觸發 Table 新增/編輯 後 Table 接收事件需要的物件樣貌

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -28,19 +28,11 @@ export class HeaderComponent implements OnInit {
 
     // ============== 新增用假資料 ==============
     const dialogData: FilterElement[] = [
-      { id: 'position', name: 'Position 查詢', value: '123', type: FilterType.NumberInput, dataSource: undefined },
+      { id: 'position', name: 'Position 查詢', value: '', type: FilterType.NumberInput, dataSource: undefined },
       { id: 'name', name: 'Name 查詢', value: '', type: FilterType.StringInput, dataSource: undefined },
       { id: 'weight', name: 'Weight 查詢', value: '', type: FilterType.StringInput, dataSource: undefined },
       { id: 'symbol', name: 'Symbol 查詢', value: '', type: FilterType.StringInput, dataSource: undefined }
     ];
-
-    // Test Data
-    // const dialogData = [
-    //   { id: 'position', name: 'Position 查詢', value: '123' },
-    //   { id: 'name', name: 'Name 查詢', value: '' },
-    //   { id: 'weight', name: 'Weight 查詢', value: '' },
-    //   { id: 'symbol', name: 'Symbol 查詢', value: '' }
-    // ];
 
     // 1.開啟 Dialog 組件視窗
     const dialogRef = this.dialog.open(EditDataSourceComponent, {
@@ -52,17 +44,8 @@ export class HeaderComponent implements OnInit {
       if (result !== undefined) {
         console.log('關閉事件後接收到的的物件資料內容', result);
 
-        // TODO 組出資料表物件
-        const add = Object.entries(dialogData).map(([key, value]) => {
-                      const keyid = Object.keys(value)[0];
-                      const id = Object.values(value)[0];
-                      const value2 = Object.values(value)[2];
-                      if (keyid === 'id' || keyid === 'value') {
-                        return { [id]: `${value2}` };
-                      }
-                    });
-
-        this.addData.emit(add);
+        const userAdd = this.convertDialogData(result);
+        this.addData.emit(userAdd);
       }
     });
   }
@@ -83,10 +66,20 @@ export class HeaderComponent implements OnInit {
       if (result !== undefined) {
         console.log('關閉事件後接收到的的物件資料內容', result);
 
-        // TODO 組出資料表物件
-        this.updateData.emit('');
+        const userEdit = this.convertDialogData(result);
+        this.updateData.emit(userEdit);
       }
     });
+  }
+
+  /** 將使用者的輸入轉化成 資料表 需要的物件 */
+  convertDialogData(arrayData) {
+    return arrayData.reduce((obj: any, item: FilterElement) => {
+      return {
+        ...obj,
+        [item.id]: [item.value],
+      };
+    }, {});
   }
 
   /** 使用者按下刪除按鈕 */
