5382150e07c6190a6d91d00f8e65b21b3f463d12
jia <jia@miniasp.com>
2020-03-06 09:58:04 +0800

    修正：TimePicker 跑版問題 修正：預設路由

--- src/app/app.router.modules.ts
+++ src/app/app.router.modules.ts
@@ -1,15 +1,14 @@
-import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { MainComponent } from './page/main/main.component';
+import { AllManagerDemoComponent } from './page/main/all-manager-demo/all-manager-demo.component';
+import { FilterTableManagerDemoComponent } from './page/main/filter-table-manager-demo/filter-table-manager-demo.component';
 import { FormManagerDemoComponent } from './page/main/form-manager-demo/form-manager-demo.component';
 import { TableManagerDemoComponent } from './page/main/table-manager-demo/table-manager-demo.component';
-import { FilterTableManagerDemoComponent } from './page/main/filter-table-manager-demo/filter-table-manager-demo.component';
-import { AllManagerDemoComponent } from './page/main/all-manager-demo/all-manager-demo.component';
 
 export const appRoutes: Routes = [
   {
-    path: '', children: [
+    path: '', component: FormManagerDemoComponent, children: [
       { path: 'form-manager-demo', component: FormManagerDemoComponent },
       { path: 'table-manager-demo', component: TableManagerDemoComponent },
       { path: 'filter-form-manager-demo', component: FilterTableManagerDemoComponent },
@@ -19,7 +18,7 @@ export const appRoutes: Routes = [
 
   // ... other routes
 
-  { path: '**', component: ErrorPageComponent}
+  { path: '**', component: ErrorPageComponent }
 ];
 
 export const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
